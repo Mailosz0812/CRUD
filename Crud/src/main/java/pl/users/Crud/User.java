9b2d67aca05c19095eb0121e163bdedf4b466ffe
Mailosz0812 <89217@student.pb.edu.pl>
2024-08-26 21:42:00 +0200

    initial

---
+++ Crud/src/main/java/pl/users/Crud/User.java
@@ -0,0 +1,32 @@
+package pl.users.Crud;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
+@ToString
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NonNull
+    private String username;
+
+    @NonNull
+    private String password;
+    public String getusername(){
+        return this.username;
+    }
+}
